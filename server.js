29d266fb95eeec2ed67069bf1918cc72be6a9308
MateosDev <35548023+MateosDev@users.noreply.github.com>
2020-01-01 17:14:04 -0600

    lets try again

--- server.js
+++ server.js
@@ -9,3 +9,18 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static(_dirname = "/app/public/"));
 
+// ================================================================================
+// ROUTER
+// ================================================================================
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// =============================================================================
+
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
